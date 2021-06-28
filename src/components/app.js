de2e5b85c5b394d6d791e7662bcf01dd9cd5d455
Matt Watson <mattwat@gmail.com>
2021-06-28 14:19:35 -0400

    finalize federation, add a bit of style

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react'
 import './app.scss'
-import { Link, Router } from '@reach/router'
+import { Link, Router, Switch } from '@reach/router'
 
+const Button = React.lazy(() => import('app1/Button'))
+const HomeView = React.lazy(() => import('app1/HomeView'))
 const SearchView = React.lazy(() => import('app1/SearchView'))
 const WorkspacesView = React.lazy(() => import('app1/WorkspacesView'))
 
@@ -9,16 +11,16 @@ export const App = () => {
   return (
     <div className="app">
       <header>
-        <Link to="/search">Search</Link> | <Link to="/workspaces">Workspaces</Link>
+        <Link to="/">Home</Link> | <Link to="/search">Search</Link> | <Link to="/workspaces">Workspaces</Link>
       </header>
-      <Router>
-        <Suspense fallback={ <div>Loading search...</div> } path="/search">
-          <SearchView />
-        </Suspense>
-        <Suspense fallback={ <div>Loading workspaces...</div> } path="/workspaces">
-          <WorkspacesView />
-        </Suspense>
-      </Router>
+      <Suspense fallback={ <div>Loading view...</div> }>
+        <Router>
+          <HomeView exact path="/" />
+          <SearchView exact path="/search" />
+          <WorkspacesView exact path="/workspaces" />
+          <HomeView default />
+        </Router>
+      </Suspense>
     </div>
   )
 }
