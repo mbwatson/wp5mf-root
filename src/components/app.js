821ff3cf9f78d686afde15ad74757fe480d23e8b
Matt Watson <mattwat@gmail.com>
2021-06-28 13:47:49 -0400

    consume app1 view components

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,17 @@ import React, { Suspense } from 'react'
 import './app.scss'
 const Button = React.lazy(() => import('app1/Button'))
 
+const SearchView = React.lazy(() => import('app1/SearchView'))
+const WorkspacesView = React.lazy(() => import('app1/WorkspacesView'))
+
 export const App = () => {
   return (
     <div className="app">
-      <Suspense fallback={ <div>Loading...</div> }>
-        <Button color="var(--color-unc)">Click Me</Button>
+      <Suspense fallback={ <div>Loading search...</div> }>
+        <SearchView />
+      </Suspense>
+      <Suspense fallback={ <div>Loading workspaces...</div> }>
+        <WorkspacesView />
       </Suspense>
     </div>
   )
