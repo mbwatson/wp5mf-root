4b5a27fb7c0d0c59deb4ea39cfee307f0090dac5
Matt Watson <mattwat@gmail.com>
2021-06-28 13:51:19 -0400

    add routing
    fix syntax error

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react'
 import './app.scss'
-const Button = React.lazy(() => import('app1/Button'))
+import { Link, Router } from '@reach/router'
 
 const SearchView = React.lazy(() => import('app1/SearchView'))
 const WorkspacesView = React.lazy(() => import('app1/WorkspacesView'))
@@ -8,12 +8,17 @@ const WorkspacesView = React.lazy(() => import('app1/WorkspacesView'))
 export const App = () => {
   return (
     <div className="app">
-      <Suspense fallback={ <div>Loading search...</div> }>
-        <SearchView />
-      </Suspense>
-      <Suspense fallback={ <div>Loading workspaces...</div> }>
-        <WorkspacesView />
-      </Suspense>
+      <header>
+        <Link to="/search">Search</Link> | <Link to="/workspaces">Workspaces</Link>
+      </header>
+      <Router>
+        <Suspense fallback={ <div>Loading search...</div> } path="/search">
+          <SearchView />
+        </Suspense>
+        <Suspense fallback={ <div>Loading workspaces...</div> } path="/workspaces">
+          <WorkspacesView />
+        </Suspense>
+      </Router>
     </div>
   )
 }
